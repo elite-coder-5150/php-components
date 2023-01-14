ed4bd042dcb0f8f852dea3d8bc9a568a939912be
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-13 23:22:10 -0600

    coded the block method

--- friend-request/classes/Relation.class.php
+++ friend-request/classes/Relation.class.php
@@ -75,4 +75,63 @@
 
             return true;
         }
+
+        public function cancel($from, $to) {
+            $sql = 'DELETE FROM relations WHERE status="pending" AND from=:from AND to=:to';
+
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                ':from' => $from,
+                ':to' => $to
+            ]);
+
+            if ($query->rowCount() == 0) {
+                $this->error = 'Invalid frind request.';
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * Unfriend - a user wants to unfriend someone.
+         * 
+         * @param int $from - the user sending the unfriend
+         * @param int $to - the user they are unfriending
+         * 
+         * @return boolean
+         */
+        public function unfriend($from, $to) {
+            $sql = 'DELETE FROM relations WHERE (
+                        status="friends" AND from=:from AND to=:to
+                    ) OR (
+                        status="friends" AND from=:to AND to=:from
+                    )';
+
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                ':from' => $from,
+                ':to' => $to
+            ]);
+
+            return true;
+        }
+
+        public function block($from, $to) {
+        
+            $sql = 'INSERT INTO relations (from, to, status) VALUES (from=:from, to=:to, status="blocked"';
+
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                ':from' => $from,
+                ':to' => $to
+            ]);
+
+            // this removes then from the friends list
+            $this->unfriend($from, $to);
+
+            return true;
+        }
+
+       
     }
