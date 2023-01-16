8f04f59d28a790f8226f8d21f8f7c43577cb9354
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-01-16 14:25:35 -0600

    started on the follow method

--- follow-system/classes/follow-system.php
+++ follow-system/classes/follow-system.php
@@ -7,13 +7,38 @@
             $this->db = new Database();
         }
 
+        public function isFollowing($get) {
+            $session = $_SESSION['user_id'];
+
+            $sql = 'SELECT * FROM follow_system WHERE follower=:follower AND following=:following';
+
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                ':follower' => $session,
+                ':following' => $get
+            ]);
+
+            if ($query->rowCount() > 0) {
+                return true;
+            }
+
+            return false;
+        }
+
        public function follow($get) {
             $utility = new Utility();
             $settings = new Settings();
             $notify = new Notification();
 
             $session = $_SESSION['user_id'];
-            // $session_u = $utility->getDetails($session);
+            $session_u = $utility->getDetails($session, "username");
+            $get_u = $utility->getDetails($get, "username");
+
+            if ($settings->amIBlocked($get) == false) {
+                if ($this->isFollowing($get) == false) {
+
+                }
+            }
        }
 
 
