1837b58d35c1a5114e0d4d2958c4c2f1bab241bc
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-18 00:47:05 -0600

    started the followers method

--- follow-system/classes/follow-system.php
+++ follow-system/classes/follow-system.php
@@ -36,10 +36,49 @@
 
             if ($settings->amIBlocked($get) == false) {
                 if ($this->isFollowing($get) == false) {
+                    $sql = "INSERT INTO follow_system(follow_by, follow_by_u, follow_to, follow_to_u, time)
+                        VALUES(:session, :session_u, :get, :get_u, now())";
 
+                    $query = $this->db->prepare($sql);
+                    $query->execute([
+                        ':session' => $session,
+                        ':session_u' => $session_u,
+                        ':get' => $get,
+                        ':get_u' => $get_u
+                    ]);
+
+                    $notify->followNotify($get, 'follow');
+
+                    return "ok";
+                } else {
+                    return 'already following';
                 }
             }
        }
+       
+    public function unfollow($get) {
+        if (self::isFollowing($get)) {
+            $session = $_SESSION['id'];
 
+            $sql = "DELETE FROM follow_system WHERE follow_by=:session AND follow_to=:get LIMIT 1";
 
+            $query = $this->db->prepare($sql);
+            $query->execute([
+                ':session' => $session,
+                ':get' => $get
+            ]);
+        }
     }
+
+    public function getFollowers($get) {
+        $sql = "SELECT follow_by FROM follow_system WHERE follow_to=:get";
+
+        $query = $this->db->prepare($sql);
+        $query->execute([
+            ':get' => $get
+        ]);
+
+        $count = $query->rowCount();
+        return $count;
+    }
+}
