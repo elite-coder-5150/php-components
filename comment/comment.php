1157c48952811bcadad0dd77f6595c9b83c30b34
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-18 14:29:53 -0600

    defined the getContent mehtod

--- comment/comment.php
+++ comment/comment.php
@@ -3,10 +3,18 @@
         protected $db;
         protected $post_id;
         protected $commentor_id;
-        
+
         public function __construct($post_id, $commentor_id) {
             $this->db = new Database();
             $this->post_id = $post_id;
             $this->commentor_id = $commentor_id;
+        }
+
+        public function getContent($post_id, $commentor_id) { 
+            $sql = "SELECT content FROM comments WHERE post_id = :post_id AND commentor_id = :commentor_id";
+            $query = $this->db->prepare($sql);
+            $query->execute([':post_id' => $post_id, ':commentor_id' => $commentor_id]);
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+            return $row['content'];
         }
     }
